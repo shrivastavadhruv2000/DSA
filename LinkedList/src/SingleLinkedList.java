0c5520fe93faedda979d4863b3ca29b9b5dc40fe
Dhruv Shrivastava <98174871+shrivastavadhruv2000@users.noreply.github.com>
2023-11-26 23:58:08 +0530

    Add files via upload

--- LinkedList/src/SingleLinkedList.java
+++ LinkedList/src/SingleLinkedList.java
@@ -72,24 +72,42 @@ public class SingleLinkedList {
             } else {
                 System.out.println("Enter 1 to delete at the beginning, 2 for at the end, 3 for at the position");
                 int m = sc.nextInt();
+                Node temp=head;
+                Node ptr=temp.next;
 
                 switch (m) {
                     case 1:
-                       Node temp=head;
+                       //Node temp=head;
                        temp=temp.next;
                        head=temp;
                        break;
                     case 2:
                         //Node temp=head;
+                        //Node ptr=temp.next;
+                        while(ptr.next!=null){
+                            temp=ptr;
+                            ptr=ptr.next;
+                        }
+                        temp.next=null;
+                        break;
+
 
 
                     case 3:
+                        System.out.println("Enter the Position to be deleted");
+                        int pos=sc.nextInt();
                         //Node temp=head;
+                        for(int i=0;i<(pos-2);i++){
+                            temp=ptr;
+                            ptr=ptr.next;
+                        }
+                        temp.next=ptr.next;
+                        break;
 
 
                 }
             }
-            System.out.print("Press 1 to add mode data: ");
+            System.out.print("Press 1 to delete mode data: ");
             n = sc.nextInt();
 
         } while (n == 1);
@@ -110,6 +128,8 @@ public class SingleLinkedList {
     public static void main(String[] args){
         SingleLinkedList l1=new SingleLinkedList();
         l1.creation();
+        l1.traverse();
+        l1.delete();
         l1.traverse();
     }
 }
