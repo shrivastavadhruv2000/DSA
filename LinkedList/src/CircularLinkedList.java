4e4e4a1b0aa85b0bd5350c9b07c6c1b278152d9e
Dhruv Shrivastava <98174871+shrivastavadhruv2000@users.noreply.github.com>
2023-11-29 01:06:32 +0530

    Add files via upload

--- LinkedList/src/CircularLinkedList.java
+++ LinkedList/src/CircularLinkedList.java
@@ -60,7 +60,63 @@ public class CircularLinkedList {
         } while (n==1);
     }
 
-    public void traverse(){
+    public void delete() {
+        int data, n;
+        Scanner sc = new Scanner(System.in);
+        do {
+
+            if (head == null) {
+                System.out.println("LL is empty");
+            } else {
+                System.out.println("Enter 1 to delete at the beginning, 2 for at the end, 3 for at the position");
+                int m = sc.nextInt();
+                Node temp=head;
+                Node ptr=temp.next;
+
+                switch (m) {
+                    case 1:
+                        //Node temp=head;
+                        head=head.next;
+                        tail.next=head;
+                        System.out.println("Start");
+                        break;
+                    case 2:
+                        //Node temp=head;
+                        //Node ptr=temp.next;
+                       while(ptr.next!=head){
+                           temp=ptr;
+                           ptr=ptr.next;
+                       }
+                       temp.next=head;
+                       tail=temp;
+                       System.out.println("End");
+                       break;
+
+
+
+                    case 3:
+                        System.out.println("Enter the Position to be deleted");
+                        int pos=sc.nextInt();
+                        //Node temp=head;
+                        for(int i=0;i<(pos-2);i++){
+                            temp=ptr;
+                            ptr=ptr.next;
+                        }
+                        temp.next=ptr.next;
+                        System.out.println("pos");
+                        break;
+
+
+                }
+            }
+            System.out.print("Press 1 to delete mode data: ");
+            n = sc.nextInt();
+
+        } while (n == 1);
+
+    }
+
+            public void traverse(){
         Node temp=head;
         if(head==null)
         {
@@ -76,6 +132,8 @@ public class CircularLinkedList {
     public static void main(String[] args){
         CircularLinkedList l1=new CircularLinkedList();
         l1.creation();
+        l1.traverse();
+        l1.delete();
         l1.traverse();
     }
 }
