00cef64358fd12736c0313d5a5bc705e4129be52
Dhruv Shrivastava <98174871+shrivastavadhruv2000@users.noreply.github.com>
2023-12-01 01:16:48 +0530

    Add files via upload

--- BinaryTree/src/BinaryTree.java
+++ BinaryTree/src/BinaryTree.java
@@ -10,17 +10,57 @@ public class BinaryTree {
         if (data == -1)
             return null;
         root=new Node(data);
-        System.out.println("enter left child of " + root);
+        System.out.println("enter left child of " + root.data);
         root.left=create();
-        System.out.println("enter right child of " + root);
+        System.out.println("enter right child of " + root.data);
         root.left=create();
 
         return root;
     }
+    static void inorder(Node root){
+        if (root==null)
+            return;
+
+            inorder(root.left);
+            System.out.print(root.data+ " ");
+            inorder(root.right);
+
+
+    }
+
+    static void preorder(Node root){
+        if (root==null)
+            return;
+
+
+        System.out.print(root.data+ " ");
+        preorder(root.left);
+        preorder(root.right);
+
+
+    }
+
+    static void postorder(Node root){
+        if (root==null)
+            return;
+
+
+
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data+ " ");
+
+
+    }
+
+
 
 
 public static void main(String[] args) {
     Node root=create();
+    inorder(root);
+    postorder(root);
+    preorder(root);
 
 }
 }
